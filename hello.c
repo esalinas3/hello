8758b01f316bea40f0684c288fac54816c79461c
Elijah Salinas <essbearcats@gmail.com>
2021-08-26 00:31:30 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Elijah Salinas
+ *         esalinas3@huskers.unl.edu
+ * Date: 2021.08.23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Elijah Salinas\n");
+
+  return 0;
+}
